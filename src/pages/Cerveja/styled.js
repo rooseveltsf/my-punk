ea7d3830c5cbf5eb375fedc31002f25ae13112d7
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-05-08 00:02:03 -0300

    add readme

--- src/pages/Cerveja/styled.js
+++ src/pages/Cerveja/styled.js
@@ -1,11 +1,11 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 export const Container = styled.div`
-    justify-content: space-around;
-    display: flex;
-    align-items: center;
-    margin-top: 40px
-`
+  justify-content: space-around;
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+`;
 
 export const SubContainer = styled.div`
     border: none;
@@ -17,46 +17,47 @@ export const SubContainer = styled.div`
     box-shadow: 5px 5px 10px #33333336;
     align-items: center;
     justify-content: center;
-`
+`;
 
 export const Info = styled.div`
-    border-radius: 20px;
-    padding: 30px;
-`
+  border-radius: 20px;
+  padding: 30px;
+`;
 
 export const Nome = styled.h2`
-    font-size: 50px;
-    margin-bottom: 20px;
-    height: 150px;
-`
+  font-size: 50px;
+  margin-bottom: 20px;
+  height: 150px;
+`;
 
 export const Tagline = styled.p`
-    font-size: 20px;
-    color: #999999c7;
-    margin-bottom: 15px;
-`
+  font-size: 20px;
+  color: #999999c7;
+  margin-bottom: 15px;
+`;
 
 export const Descricao = styled.p`
-    width: 500px;
-    font-size: 19px;
-`
+  width: 500px;
+  font-size: 19px;
+`;
 
 export const Imagem = styled.img`
-    width: 100px;
-    height: 400px;
-    padding: 20px;
-`
+  width: 100px;
+  height: 400px;
+  padding: 20px;
+`;
 
 export const Botao = styled.button`
-    background: #fff;
-    color: #333;
-    border: none;
-    border-radius: 50px;
+  background: #fff;
+  color: #333;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
 
-    &:hover {
-        opacity: 0.3
-    }
-    &:disabled {
-        opacity: 0.3
-    }
-`
+  &:hover {
+    opacity: 0.3;
+  }
+  &:disabled {
+    opacity: 0.3;
+  }
+`;
